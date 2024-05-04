1a7caa0a1f737c843c66b9c8c260c2c4e27b4849
ozzyozbourne <khanosaid726@gmail.com>
2024-05-04 17:32:20 -0400

    window working fine

--- src/main.zig
+++ src/main.zig
@@ -2,51 +2,69 @@ const print = @import("std").debug.print;
 const glfw3 = @import("glfw3.zig");
 const glew = @import("glew.zig");
 
+const WIDTH: glfw3.GLint = 800;
+const HEIGHT: glfw3.GLint = 600;
+
 pub fn main() !void {
-    var major: c_int = undefined;
-    var minor: c_int = undefined;
-    var patch: c_int = undefined;
-
-    glfw3.glfwGetVersion(&major, &minor, &patch);
-
-    print("major -> {d}, minor -> {d}, patch -> {d}\n", .{ major, minor, patch });
-
-    const glfw_err: c_int = glfw3.glfwInit();
-    print("{}\n", .{glfw_err});
-
-    if (glfw_err != glfw3.GLFW_TRUE) {
-        print("glwf init error is not zero {d}\n", .{glfw_err});
+    // initilise GLFW
+    const init_error: c_int = glfw3.glfwInit();
+    if (init_error == glfw3.GLFW_TRUE) {
+        print("GLFW initilized successfully -> {d}\n", .{init_error});
+    } else {
+        print("unable to init GLFW\n", .{});
+        glfw3.glfwTerminate();
         return;
     }
 
+    // Setup GLFW window properties
+    // Opengl version
     glfw3.glfwWindowHint(glfw3.GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfw3.glfwWindowHint(glfw3.GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core profile = No backwards Compatibility
     glfw3.glfwWindowHint(glfw3.GLFW_OPENGL_PROFILE, glfw3.GLFW_OPENGL_CORE_PROFILE);
+    // Allow forward compatibility
+    glfw3.glfwWindowHint(glfw3.GLFW_OPENGL_FORWARD_COMPAT, glfw3.GLFW_TRUE);
 
-    const window: ?*glfw3.GLFWwindow = glfw3.glfwCreateWindow(400, 400, "test", null, null);
-    defer glfw3.glfwDestroyWindow(window);
-
+    const window: ?*glfw3.GLFWwindow = glfw3.glfwCreateWindow(WIDTH, HEIGHT, "Test Window", null, null);
     if (window == null) {
-        print("Error -> {?}\n", .{window});
-    } else {
-        print("{?}\n", .{window});
+        print("GLFW window creation failed", .{});
+        glfw3.glfwTerminate();
+        return;
     }
 
+    //Get buffer size information
+    var bufferWidth: c_int = undefined;
+    var bufferHeight: c_int = undefined;
+    glfw3.glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
+
+    //Set context for GLEW to use
     glfw3.glfwMakeContextCurrent(window);
+
+    // Allow modern extensions features
     glew.glewExperimental = glew.GL_TRUE;
 
-    const glew_err: glew.GLenum = glew.glewInit();
-
-    if (glew_err == glew.GLEW_OK) {
-        print("All good\n", .{});
+    const glew_error: glew.GLenum = glew.glewInit();
+    if (glew_error == glew.GLEW_OK) {
+        print("GLEW initilized successfully {d}\n", .{glew_error});
+    } else {
+        print("GLEW initilized failed {}\n", .{glew_error});
+        glfw3.glfwDestroyWindow(window);
+        glfw3.glfwTerminate();
+        return;
     }
 
-    const version: [*c]const glew.GLubyte = glew.glGetString(glew.GL_VERSION);
+    //Setup Viewport size
+    glew.glViewport(0, 0, bufferWidth, bufferHeight);
 
-    print("{s}\n", .{version});
-
+    //Loop until the window closed
     while (glfw3.glfwWindowShouldClose(window) != 1) {
-        glfw3.glfwSwapBuffers(window);
+        // Get + Handle user input events
         glfw3.glfwPollEvents();
+
+        //clear window
+        glfw3.glClearColor(@as(f32, 1.0), @as(f32, 0.0), @as(f32, 0.0), @as(f32, 1.0));
+        glfw3.glClear(glfw3.GL_COLOR_BUFFER_BIT);
+
+        glfw3.glfwSwapBuffers(window);
     }
 }
